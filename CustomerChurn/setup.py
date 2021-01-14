3f2fe8c7cbd2b477ec691dd9ae1992d942545770
Juan Malaver <jmalaver@ecapitaladvisors.com>
2021-01-14 11:35:03 -0600

    Churn Template

---
+++ CustomerChurn/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Quick-start template for financial services churn analysis',
+    author='Juan Malaver',
+    license='MIT',
+)
