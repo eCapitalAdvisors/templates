6376a5c33ee5efa58339854d213323733d55e772
lila sahar <lsahar@ecapitaladvisors.com>
2021-11-19 13:00:05 -0600

    I have finished the static flexdashboard!!

---
+++ Elasticity/R/plot_fitted_vs_residual.R
@@ -0,0 +1,28 @@
+# Creation of fitted vs residual plot
+
+plot_fitted_vs_residual <- function(sales_sample_tbl, model = "none", method = "ML") {
+  
+  if (model == "REM") {
+    lmfit <- lm(log(sales) ~ log(price) + description, data = sales_sample_tbl)
+    
+    p <- ggplot(sales_sample_tbl, aes(lmfit$fitted.values, lmfit$residuals)) +
+      geom_point() +
+      geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+      labs(title = "Fitted vs Residuals", x = "Residuals", y = "Fitted Values") +
+      theme(plot.title = element_text(hjust = .5, face = "bold"))
+    
+    ggplotly(p)
+  }
+  
+  else if (model == "MEM") {
+    lmfit <- lm(log(sales) ~ log(price) * description, data = sales_sample_tbl)
+    
+    p <- ggplot(sales_sample_tbl, aes(lmfit$fitted.values, lmfit$residuals)) +
+      geom_point() +
+      geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+      labs(title = "Fitted vs Residuals", x = "Residuals", y = "Fitted Values") +
+      theme(plot.title = element_text(hjust = .5, face = "bold"))
+    
+    ggplotly(p)
+  }
+}
